553360876da9c1eae44d5bbe849a333bcb399fc1
rughjake <60996295+rughjake@users.noreply.github.com>
2020-05-07 15:18:13 -0700

    Add files via upload

---
+++ rootalias.C
@@ -0,0 +1,2 @@
+#include "BEvent.cc"
+#include "BParticle.cc"
---
+++ startroot.bat
@@ -0,0 +1,8 @@
+
+set TEMP=c:\temp
+set TMPDIR=c:\temp
+set HOME="c:\Documents and Settings\nozaki\My Documents\b-lab"
+set ROOTSYS=c:\root
+set PATH=%PATH%;%ROOTSYS%\bin
+
+
